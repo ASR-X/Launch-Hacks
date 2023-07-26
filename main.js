6c953976a6d27ecd7d5532867338cafc3027adb3
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2023-07-26 01:00:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Action items: fix font color under "results", fix longitude error
+
 document.getElementById('planningForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const data = {
@@ -10,7 +12,7 @@ document.getElementById('planningForm').addEventListener('submit', function(e) {
     axios.get('http://localhost:8000/trip', { params: data })
         .then(function(response) {
             document.getElementById('results').classList.remove('d-none');
-            // displayItinerary(response.data.itinerary);
+            displayItinerary(response.data.itinerary);
             createMiniMap(response.data.itinerary);
         })
         .catch(function(error) {
@@ -18,70 +20,50 @@ document.getElementById('planningForm').addEventListener('submit', function(e) {
         });
 });
 
-// function displayItinerary(itinerary) {
-//     let content = '<h style = "color: white;"> A brief overview of your travel plan: </h>';
-//     for (const city in itinerary) {
-//         const data = itinerary[city][0];
-//         content += `<h2 style = "color: white">${city}</h2>
-//                     <p style = "color: white">Stay at the <strong>${data.accommodations[0]}</strong> for <strong>${data['number of days']}</strong></p>
-//                     <p style = "color: white">Activities:</p>
-//                     <ul style = "color: white">`;
-//         data.activities.forEach(activity => {
-//             content += `<li>Go to the <strong>${activity[0]}</strong></li>`;
-//         });
-//         content += '</ul>';
-//         const containerId = 'mapContainer' + city;
-//         const newDiv = document.createElement('div');
-//         newDiv.id = containerId;
-//         newDiv.style.height = '400px';
-//         newDiv.style.width = '600px';
-//         newDiv.style.margin = '20px';
+function displayItinerary(itinerary) {
+  for (const city in itinerary) {
+    const data = itinerary[city][0];
 
-//         const existingDiv = document.getElementById("mainBody");
-//         existingDiv.appendChild(newDiv);
-//         document.getElementById(containerId).innerHTML = content;
-//     }
-//     // document.getElementById('results').innerHTML = content;
-// }
+    // Create new div for this city's results
+    const resultsDiv = document.createElement('div');
+    resultsDiv.id = city + 'Results';
+    resultsDiv.classList.add('city-results');
+    document.getElementById('results').appendChild(resultsDiv);
+
+    // Add itinerary to this city's results div
+    let content = `<h2 style = "color: white">${city}</h2>
+                   <p style = "color: white">Stay at <strong>${data.accommodations[0]}</strong> for <strong>${data['number of days']}</strong></p>
+                   <p style = "color: white">Activities:</p>
+                   <ul style = "color: white">`;
+    data.activities.forEach(activity => {
+      content += `<li>Go to the ${activity[0]}</li>`;
+    });
+    content += '</ul>';
+    resultsDiv.innerHTML = content;
+
+    // Now add map container to this city's results div
+    const mapContainer = document.createElement('div');
+    mapContainer.id = 'mapContainer' + city;
+    mapContainer.style.height = '400px';
+    mapContainer.style.width = '600px';
+    mapContainer.style.margin = '20px';
+    resultsDiv.appendChild(mapContainer);
+  }
+}
 
 function createMiniMap(itinerary){
-    
     for (const city in itinerary){
-        let content = '';
-
-        var containerIdDescription = 'mapContainer' + city + 'Description';
-        var newDiv = document.createElement('div');
-        newDiv.id = containerIdDescription;
-        newDiv.style.height = '200px';
-        newDiv.style.width = '600px';
-        newDiv.style.margin = '20px';
-
-        var existingDiv = document.getElementById("results");
-        existingDiv.appendChild(newDiv);
-
-        const data = itinerary[city][0];
-        
-        content += `<h2 style = "color: white">${city}</h2>
-                    <p style = "color: white">Stay at the <strong>${data.accommodations[0]}</strong> for <strong>${data['number of days']}</strong></p>
-                    <p style = "color: white">Activities:</p>
-                    <ul style = "color: white">`;
-        data.activities.forEach(activity => {
-            content += `<li>Go to the <strong>${activity[0]}</strong></li>`;
-        });
-        content += '</ul>';
-
-        document.getElementById(containerIdDescription).innerHTML = content;
-
-        // Create Map
-
-        var containerIdMap = 'mapContainer' + city + 'Map';
-        var newDiv = document.createElement('div')
-        newDiv.id = containerIdMap;
+        const containerId = 'mapContainer' + city;
+        const newDiv = document.createElement('div');
+        newDiv.id = containerId;
         newDiv.style.height = '400px';
         newDiv.style.width = '600px';
         newDiv.style.margin = '20px';
 
+        const existingDiv = document.getElementById("mainBody");
         existingDiv.appendChild(newDiv);
+
+        const data = itinerary[city][0];
         
         const coordsAccommodation = data['accommodations'][1];
         const [accommodationLat, accommodationLong] = coordsAccommodation.split(','); // Might have to change brackets
@@ -92,7 +74,7 @@ function createMiniMap(itinerary){
         const initialCenter = { lat: accommodationLatFloat, lng: accommodationLongFloat};
         const accommodationLabel = data.accommodations[0];
         const accommodationImageUrl = data.accommodations[2];
-        const accommodationImgString = `<h1 id="firstHeading" class="firstHeading" style="font-weight:bold; font-size: 14px; color: black;">${accommodationLabel}</h1><div><img src="${accommodationImageUrl}" alt="Image" style="max-width: 100%;"></div>`;
+        const accommodationImgString = `<h1 id="firstHeading" class="firstHeading" style="font-weight:bold; font-size: 14px;">${accommodationLabel}</h1><div><img src="${accommodationImageUrl}" alt="Image" style="max-width: 100%;"></div>`;
         const accommodationPointsItem = { lat: accommodationLatFloat, lng: accommodationLongFloat, img: accommodationImgString };
 
         const points = [];
@@ -107,17 +89,17 @@ function createMiniMap(itinerary){
             const activityLongFloat = parseFloat(activityLong);
 
             const activityImgUrl = activity[2]
-            const activityImgString = `<h1 id="firstHeading" class="firstHeading" style="font-weight:bold; font-size: 14px; color: black;">${activityLabel}</h1><div><img src="${activityImgUrl}" alt="Image" style="max-width: 100%;"></div>`;
+            const activityImgString = `<h1 id="firstHeading" class="firstHeading" style="font-weight:bold; font-size: 14px;">${activityLabel}</h1><div><img src="${activityImgUrl}" alt="Image" style="max-width: 100%;"></div>`;
 
             const activityPointsItem = { lat: activityLatFloat, lng: activityLongFloat, img: activityImgString };
             points.push(activityPointsItem);
 
         });
 
-        const map = new google.maps.Map(document.getElementById(containerIdMap), {
-            center: initialCenter,
-            zoom: 12,
-        });
+        const map = new google.maps.Map(document.getElementById('mapContainer' + city), {
+        center: initialCenter,
+        zoom: 12,
+    });
 
         points.forEach(point => {
             const marker = new google.maps.Marker({
